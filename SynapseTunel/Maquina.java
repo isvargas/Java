d1eb6b40505b294f5a62271f4859cd0817afc8dd
Ivan Vargas <contato@is5.com.br>
2017-10-26 10:19:39 -0200

    Synapse Tunel TCP/IP

---
+++ SynapseTunel/Maquina.java
@@ -0,0 +1,61 @@
+/*
+ * SynapseTunel - Tunel TCP/IP em Java
+ * Ivan S. Vargas
+ * contato@is5.com.br
+ */
+package synapsetunel;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Ivan Vargas
+ */
+public class Maquina {
+
+    private int ID;
+    private Socket cliente;
+
+    public void setCliente(Socket cliente) {
+        this.cliente = cliente;
+    }
+    private String alias;
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public Socket getCliente() {
+        return cliente;
+    }
+
+    public String getIp() {
+        return this.cliente.getInetAddress().getHostAddress();
+    }
+
+    public String getNome() {
+        return this.cliente.getInetAddress().getHostName();
+    }
+
+    public Maquina(Socket client){
+        this.cliente = client;        
+    }
+    
+        public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    
+    @Override
+    public String toString(){
+        return this.alias;
+    }    
+    
+}
---
+++ SynapseTunel/SynapseTunel.java
@@ -0,0 +1,323 @@
+/*
+ * SynapseTunel - Tunel TCP/IP em Java
+ * Ivan S. Vargas
+ * contato@is5.com.br
+ */
+package synapsetunel;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ivan Vargas
+ */
+public class SynapseTunel {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private int IdCliente = -1;
+    
+    private ArrayList<Maquina> maquinas = new ArrayList();
+    private ServerSocket ClienteServer, AdminServer;
+    private Socket ClienteSocket, AdminSocket;
+    
+    private DataInputStream inAdmin = null;
+    private OutputStreamWriter outAdmin = null;
+    
+    synchronized public void addMaquina(Socket client) {
+        boolean vinculada = false;
+        
+        /* procura na lista se nao tinha uma maquina com mesmo IP e Hostname. Se tiver... vincula o socket... */
+        for(Maquina m : maquinas) {
+            if( (m.getIp().equals(client.getInetAddress().getHostAddress())) && (m.getNome().equals(client.getInetAddress().getHostName())) ) {
+                try {
+                    m.getCliente().close();
+                    m.setCliente(client);
+                    vinculada = true;
+                    break;
+                } catch (Exception ex) {
+                    vinculada = false;
+                    System.out.println("Erro ao vincular socket.");
+                }
+            }
+        }
+        
+        /* ... do contrario, adiciona */
+        if(!vinculada) {
+            Maquina m = new Maquina(client);
+            m.setID(maquinas.size());
+            m.setAlias(m.getID() + " - " + m.getIp());
+            maquinas.add(m);
+        }
+    }
+    
+    synchronized public void removeMaquina(Integer IdMaquina) {
+        for(Maquina m : maquinas) {
+            try
+            {
+                if (m.getID() == IdMaquina) {
+                    m.getCliente().close();
+                    maquinas.remove(m);
+                }
+            }catch(Exception ex) {
+                System.out.println("Erro ao remover maquina: " + ex.getMessage());
+            }
+        }
+    }
+    
+    
+    public SynapseTunel(){
+        new ClienteServerConnect().start();
+        new AdminServerConnect().start();
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new SynapseTunel();
+    }
+    
+    class ClienteServerConnect extends Thread {
+        
+        public void run() {
+            try {
+                ClienteServer = new ServerSocket(3281);
+                
+                System.out.println("Servidor de Clientes iniciado na porta 3281.\nAguardando conexao.");
+                
+                while(true) 
+                {
+                    ClienteSocket = ClienteServer.accept();
+                    System.out.println("Cliente conectado no servidor.");
+                
+                    ClienteThread ct = new ClienteThread(ClienteSocket);
+                    ct.start();
+                }               
+                
+            } catch (Exception ex) {
+                System.out.println("Erro ao iniciar servidor de clientes: " + ex.getMessage());
+            }
+        }
+    }
+    
+    class ClienteThread extends Thread {
+        
+        private Socket client;
+        
+        public ClienteThread(Socket client) {
+            this.client = client;
+            addMaquina(this.client);
+        }
+        
+        public void run() {
+            
+            try {
+                
+                DataInputStream in = new DataInputStream(client.getInputStream());
+                OutputStreamWriter outw = new OutputStreamWriter(client.getOutputStream());
+                int data;
+                String resposta = null;
+                StringBuffer buffer = new StringBuffer();
+                while (true) 
+                {
+                    while ((data = in.read()) != 10)
+                    {
+                        buffer.append((char) data);
+                    } 
+                    resposta = buffer.toString();
+                    buffer.delete(0, buffer.length());
+                    System.out.println(resposta);
+                    if (outAdmin != null) 
+                    {
+                        outAdmin.write(resposta+"\n\r");
+                        outAdmin.flush();
+                    }                    
+                }           
+                
+            }catch(Exception ex) {
+                System.out.println("Erro em cliente: " + ex.getMessage());
+            }          
+            
+        }
+    }
+    
+    class AdminServerConnect extends Thread {
+        
+        public void run() {
+            try {
+                AdminServer = new ServerSocket(3282);
+                
+                System.out.println("Servidor Admin iniciado na porta 3282.\nAguardando conexao.");
+                
+                while(true) 
+                {
+                    AdminSocket = AdminServer.accept();
+                    System.out.println("Admin conectado no servidor.");
+                
+                    AdminThread at = new AdminThread(AdminSocket);
+                    at.start();
+                }               
+                
+            } catch (Exception ex) {
+                System.out.println("Erro ao iniciar servidor de clientes: " + ex.getMessage());
+            }
+        }
+		
+    }
+    
+    class AdminThread extends Thread {
+        
+        private Socket admin;
+        private StringBuffer buffer = new StringBuffer();
+        private int data;
+        
+        public AdminThread(Socket admin) {
+            this.admin = admin;
+        }
+        
+        private String getMaquinas() {
+            String s = "";
+            if (maquinas.size() > 0) {
+                for(Maquina m : maquinas) {
+                    s += m.getAlias() + "\n";
+                }
+            }
+            if(s.isEmpty())
+                s = "Nenhuma maquina conectada neste momento.";                 
+            return s;
+        }
+        
+        private boolean Send(String mensagem) 
+        {
+            try 
+            {
+                outAdmin.write(mensagem.trim()+"\n\r");
+                outAdmin.flush();
+                return true;
+            } catch (Exception ex) {
+                System.out.println("Erro ao enviar mensagem: " + ex.getMessage());
+                return false;
+            }
+        }
+        
+        private Maquina getMaquinaPorId(int ID) {
+            Maquina maquina = null;
+            for(Maquina m : maquinas) {
+                if(m.getID() == ID) {
+                    maquina = m;
+                }
+            }
+            return maquina;
+        }
+
+        private String Received() 
+        {
+            try 
+            {                
+                while ((data = inAdmin.read()) != 10) 
+                {
+                    buffer.append((char) data);
+                }
+                String s = buffer.toString();
+                buffer.delete(0, buffer.length());  
+                return s;
+            } catch (Exception ex) {
+                System.out.println("Erro ao receber mensagem: " + ex.getMessage());
+                return null;
+            }
+        }
+        
+        private boolean SendComandoPorMaquina(Maquina m, String cmd) {
+            try {
+                OutputStreamWriter out = new OutputStreamWriter(m.getCliente().getOutputStream());
+                out.write(cmd+"\n\r");
+                out.flush();
+                return true;
+            }catch(Exception ex) {
+                System.out.println("Erro ao enviar comando para maquina: " + ex.getMessage());
+                Send("Erro");
+                return false;
+            }
+        }
+        
+        public void run() {
+            try {
+                inAdmin = new DataInputStream(admin.getInputStream());
+                outAdmin = new OutputStreamWriter(admin.getOutputStream());
+                
+                String cmd = "";
+                
+                Send("************************************************\n"
+                   + "             BEM VINDO AO SYNAPSE               \n"
+                   + "************************************************\n\n");
+                
+                while(true) 
+                {
+                    cmd = Received().replace('\n', ' ').replace('\r', ' ').trim();
+                    
+                    System.out.println("Recebido: " + cmd);
+                    
+                    if (cmd.toLowerCase().startsWith("lsm")) {
+                        String lm = getMaquinas();
+                        System.out.println("Maquinas: " +lm);
+                        Send(lm);                        
+                    } else if (cmd.toLowerCase().startsWith("connect:")) {
+                        try {
+                            IdCliente = Integer.parseInt(cmd.substring(8, cmd.length()));
+                            SendComandoPorMaquina(getMaquinaPorId(IdCliente), "hostname");
+
+                        } catch (Exception ex) {
+                            IdCliente = -1;
+                            System.out.println("Erro ao conectar no cliente: " + ex.getMessage());
+                            Send("Erro ao conectar no cliente.");
+                        }
+                    }
+                    else if (cmd.toLowerCase().startsWith("disconnect")) {
+                        IdCliente = -1;
+                        Send("Desconectado da maquina remota.");
+                    }
+                    else if (cmd.toLowerCase().startsWith("remove:")) {
+                        try {
+                            int id = Integer.parseInt(cmd.substring(7, cmd.length()));
+                            removeMaquina(id);
+
+                        } catch (Exception ex) {
+                            System.out.println("Erro ao remover maquina: " + ex.getMessage());
+                            Send("Maquina removida.");
+                        }
+                    }
+                    else if (cmd.toLowerCase().startsWith("info")) {
+                        String info = "";
+                        if(IdCliente > -1) {
+                            info += "\nVoce esta conectado na maquina: " + IdCliente;
+                            info += "\nEndereco IP: " + getMaquinaPorId(IdCliente).getIp();
+                            info += "\nHostname: " + getMaquinaPorId(IdCliente).getNome();
+                            Send(info);
+                        }
+                        else
+                            Send("Voce nao esta conectado em nenhuma maquina");
+                        
+                    }
+                    else {
+                        if (IdCliente > -1) {
+                           SendComandoPorMaquina(getMaquinaPorId(IdCliente), cmd); 
+                        }
+                    }
+                    
+                }				
+                
+            }catch(Exception ex) {
+                System.out.println("Erro ao Admin: " + ex.getMessage());
+            }
+        }
+        
+    }
+    
+}
